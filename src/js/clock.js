3fdeb608203821fa047ed3feb799d3c7cb5f0cec
John Chase <ratteal@gmail.com>
2020-06-03 12:05:17 +0100

    Simplify hand angle mapping and movement.

--- src/js/clock.js
+++ src/js/clock.js
@@ -9,37 +9,38 @@ class Clock {
         this.setTarget();
     }
 
-    _drawHand(angle) {
+    _drawHand(hand) {
         push();
         translate(this.x, this.y);
-        rotate(angle - 90);
+        rotate((hand * 6) - 90);
         strokeWeight(4);
-        line(0, 0,this.d * 8 / 20, 0);
+        line(0, 0,this.d * 2 / 5, 0);
         pop();
     }
 
     render() {
         stroke(0);
-        fill(240);
+        noFill();
         ellipse(this.x, this.y, this.d);
-        this._drawHand(map(this.mm, 0, 60, 0, 360));
-        this._drawHand(map(this.hh, 0, 60, 0, 360));
+        stroke(50);
+        fill(240);
+        ellipse(this.x, this.y, this.d - 4);
+        this._drawHand(this.mm);
+        this._drawHand(this.hh);
     }
 
     update() {
         if (this.hh !== this.toHH) {
-            this.hh = this.hh + this.deltaHH;
+            this.hh = (this.hh + 1) % 60;
         }
         if (this.mm !== this.toMM) {
-            this.mm = this.mm + this.deltaMM;
+            this.mm = (this.mm + 1) % 60;
         }
     }
 
     setTarget() {
         this.toHH = int(random(0, 60));
         this.toMM = int(random(0, 60));
-        this.deltaHH = this.toHH > this.hh ? 1 : -1;
-        this.deltaMM = this.toMM > this.mm ? 1 : -1;
     }
 
 }
