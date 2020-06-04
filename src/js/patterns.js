9040b12f7ac007e55a6e20f426e302de9f8f2f43
John Chase <ratteal@gmail.com>
2020-06-04 21:20:47 +0100

    These patterns look better.

--- src/js/patterns.js
+++ src/js/patterns.js
@@ -8,12 +8,12 @@ const PATTERNS = {
         {
             "width": 2,
             "height": 2,
-            "layout": ["BBBBBBBBB", "BAAAAAAAB", "BAAAAAAAB", "BBBBBBBBB"]
+            "layout": ["BBBBBBBBB", "BBBBBBBBB", "BBBBBBBBB", "BBBBBBBBB"]
         },
         {
             "width": 2,
             "height": 2,
-            "layout": ["CCCCCCCCC", "CCCCCCCCC", "CCCCCCCCC", "CCCCCCCCC"]
+            "layout": ["ABABABABA", "BABABABAB", "ABABABABA", "BABABABAB"]
         }
     ],
     "A": {
@@ -28,16 +28,8 @@ const PATTERNS = {
         "width": 2,
         "height": 2,
         "shape": [
-            {"hh": 0,  "mm": 45}, {"hh": 0,  "mm": 15},
-            {"hh": 30, "mm": 45}, {"hh": 15, "mm": 30}
-        ]
-    },
-    "C": {
-        "width": 2,
-        "height": 2,
-        "shape": [
-            {"hh": 50, "mm": 55}, {"hh": 5,  "mm": 10},
-            {"hh": 35, "mm": 40}, {"hh": 20, "mm": 25}
+            {"hh": 20, "mm": 25}, {"hh": 35, "mm": 40},
+            {"hh": 5,  "mm": 10}, {"hh": 50, "mm": 55}
         ]
     }
 }
